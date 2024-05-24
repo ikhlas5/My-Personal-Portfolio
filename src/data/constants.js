3de249430eab7c8273fb8c9a0e586489d210feee
Omar Faruk <omarfaruk9565@gmail.com>
2024-05-24 18:24:34 +0600

    update  commit

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/ikhlas5",
   resume:
-    "https://drive.google.com/file/d/1YUjuCZeUlhng47fhtQN718FwLixHGsmf/view?usp=sharing",
+    "https://drive.google.com/file/d/1UPqtFeJ5VS-2WblVTldyJ3Iz2iett4Ou/view?usp=sharing",
   linkedin: "https://www.linkedin.com",
   twitter: "https://twitter.com",
   insta: "https://www.instagram.com/mahmudulikhlas/",
